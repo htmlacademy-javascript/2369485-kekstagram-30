b1cd10205caea4aa93c4481e144c157af0e56eec
Astsatur <ogandzanov2@mail.ru>
2023-10-10 17:03:18 +0300

    Исправляет после проверки наставником

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,17 @@
-const checksLineLength = (string, maxLength) => (string.length >= maxLength);
+const checkLineLength = (string, maxLength) => (string.length >= maxLength);
 
-checksLineLength();
+checkLineLength();
 
-const checksPalindomority = (string) => {
+const checkForPalindrom = (string) => {
   const newString = string.replaceAll(' ', '').toUpperCase();
   let emptyString = '';
-  for(let i = newString.length - 1; i >= 0; i--) {
+  for (let i = newString.length - 1; i >= 0; i--) {
     emptyString = emptyString + newString.at(i);
   }
   if (newString === emptyString) {
     return true;
-  }else {
-    return false;
   }
+  return false;
 };
 
-checksPalindomority();
+checkForPalindrom();
